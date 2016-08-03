92f1bc8f2099baa6d561058953433fadbc7dc0cb
Le Deng <lede@Les-MacBook-Pro.local>
2016-08-03 15:39:57 -0400

    logging

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,6 +1,8 @@
 package com.devopsbuddy.web.i18n;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -15,6 +17,9 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+    
     @Autowired
     private MessageSource messageSource;
 
